9b2b2f7ed8c37cb631b78e08155b01121e5ee68a
MCX <ctkhoule@gmail.com>
2020-02-06 10:57:55 +0000

    Commit Todolist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,33 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB2Hgp20iMiTNDAHToSNrwaX3xkqF5t3AU",
+  authDomain: "todolistionic-4f83a.firebaseapp.com",
+  databaseURL: "https://todolistionic-4f83a.firebaseio.com",
+  projectId: "todolistionic-4f83a",
+  storageBucket: "todolistionic-4f83a.appspot.com",
+  messagingSenderId: "377259368536",
+  appId: "1:377259368536:web:2c30995740df1c19732a2d",
+  measurementId: "G-5EWRB91CWR"
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
@@ -21,3 +41,4 @@ import { AppRoutingModule } from './app-routing.module';
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//importer le module AngularFireDatabase
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,70 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  //Declaration de la variable currentDate
+  currentDate: string;
+  const
+  date = new Date();
+
+  myTask = '';
+  //a variable booléenne addTask qui servira de condition d’affichage dans le HTML
+  addTask: boolean;
+//stocker toutes les informations de nos tâches:
+  tasks = [];
+
+  constructor(public afDB: AngularFireDatabase) {
+    const date = new Date();
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    this.currentDate = date.toLocaleDateString('fr-FR', options);
+    this.getTasks();
+
+  }
+
+  /**
+   * Cette fonction ajoute un nouvel item à notre table Tasks,
+   * en lui donnant les informations suivantes: le texte, la date, et l’état de la tâche
+   */
+  addTaskToFirebase() {
+    this.afDB.list('Tasks/').push({
+      text: this.myTask,
+      date: new Date().toISOString(),
+      checked: false
+    });
+    this.showForm();
+  }
+
+  /**
+   * la fonction showForm() dont le but sera de changer la valeur de cette variable booléenne.
+   */
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+
+  /**
+   * pour parcourir notre base firebase Tasks/ et récupérer mes informations de nos tâches:
+   */
+  getTasks() {
+    this.afDB.list('Tasks/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        this.tasks.push({
+          key: action.key,
+          text: action.payload.exportVal().text,
+          hour: action.payload.exportVal().date.substring(11, 16),
+          checked: action.payload.exportVal().checked
+        });
+      });
+    });
+  }
+
+  changeCheckState(ev: any) {
+    console.log('checked: ' + ev.checked);
+    this.afDB.object('Tasks/' + ev.key + '/checked/').set(ev.checked);
+  }
+
+  deleteTask(task: any) {
+    this.afDB.list('Tasks/').remove(task.key);
+  }
 
 }
